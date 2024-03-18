e68663c1249be4d8e1e4928178dec985c68a6e29
petethepossum <47347759+petethepossum@users.noreply.github.com>
2024-03-18 17:58:28 +1300

    Update dbConfig.js to include PORT CONFIG support

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,6 +2,7 @@ module.exports = {
     HOST: process.env.DB_HOST || '0.0.0.0', // or change this to just 127.0.0.1
     USER: 'root',
     PASSWORD: 'password',
+    PORT: '3306', // change this to your DB port
     DB: 'account',
     dialect: 'mariadb', // change dialect to mysql if you're not using mariadb
     pool: {
